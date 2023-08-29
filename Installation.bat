4f43c1ac1fab5cbab812ada15833324d48211451
sprakhar11 <prakhar.infinity@gmail.com>
2023-08-29 14:58:08 +0530

    folder fixed

--- Installation.bat
+++ Installation.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Open the first command prompt and execute a command
-start "Command Prompt 3" cmd /k "git clone "
+start "Command Prompt 3" cmd /k "git clone https://github.com/sprakhar11/jssaten_invoice_gen.git"
 
 
 :: Wait for a few seconds to allow the command prompts to open and execute
